9c27efa53865954976fe4e426a0b96419efa4a8d
minami <sourcechord@gmail.com>
2016-02-27 15:23:40 +0900

    Add usages to LightySample project

--- Sample/LightySample/MainWindow.xaml.cs
+++ Sample/LightySample/MainWindow.xaml.cs
@@ -26,15 +26,45 @@ namespace LightySample
             InitializeComponent();
         }
 
-        private async void button_Click(object sender, RoutedEventArgs e)
+        private void OnClickShowButton(object sender, RoutedEventArgs e)
+        {
+            LightBox.Show(this, new SampleDialog());
+            MessageBox.Show("Hello.");
+        }
+
+        private void OnClickShowDialogButton(object sender, RoutedEventArgs e)
         {
             LightBox.ShowDialog(this, new SampleDialog());
+            MessageBox.Show("Hello.");
+        }
 
+        private async void OnClickShowAsyncButton(object sender, RoutedEventArgs e)
+        {
+            await LightBox.ShowAsync(this, new SampleDialog());
+            MessageBox.Show("Hello.");
+        }
+
+        private async void OnClickShowMultiple(object sender, RoutedEventArgs e)
+        {
+            LightBox.Show(this, new SampleDialog());
+
+            MessageBox.Show("Hello.");
             await Task.Delay(1000);
             LightBox.Show(this, new SampleDialog());
 
             await Task.Delay(1000);
             LightBox.Show(this, new SampleDialog());
+        }
+
+        private void OnClickShowUserControl(object sender, RoutedEventArgs e)
+        {
+            LightBox.Show(this, new SampleDialog());
+        }
+        private void OnClickShowImage(object sender, RoutedEventArgs e)
+        {
+            var image = new Image();
+            image.Source = new BitmapImage(new Uri("Images/1.jpg", UriKind.Relative));
+            LightBox.Show(this, image);
         }
     }
 }
