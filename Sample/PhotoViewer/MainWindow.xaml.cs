0574ff51a4926abb95e4da683294261cb24ce2ba
minami <sourcechord@gmail.com>
2016-03-20 12:42:14 +0900

    Add PhotoViewer sample

---
+++ Sample/PhotoViewer/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using SourceChord.Lighty;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoViewer
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // DataContext変更時にViewModelプロパティの値が変わるように
+            // イベントハンドラの設定
+            this.ViewModel = this.DataContext as MainWindowViewModel;
+            this.DataContextChanged += (s, e) =>
+            {
+                this.ViewModel = this.DataContext as MainWindowViewModel;
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dlg = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                var ret = dlg.ShowDialog();
+                if (ret != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+
+                // 選択されたフォルダ内にある画像を列挙
+                this.ViewModel.SetSourceDirectory(dlg.SelectedPath);
+            }
+        }
+
+        private void OnItemDoubleClicked(object sender, MouseEventArgs e)
+        {
+            var helper = new System.Windows.Interop.WindowInteropHelper(App.Current.MainWindow);
+            var screen = System.Windows.Forms.Screen.FromHandle(helper.Handle);
+            var height = screen.Bounds.Height;
+
+            var path = this.lstPhoto.SelectedItem as Uri;
+            var bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.UriSource = path;
+            bmp.DecodePixelHeight = height;
+            bmp.EndInit();
+
+            var image = new Image() { Source = bmp};
+            LightBox.Show(this, image);
+        }
+    }
+}
---
+++ Sample/PhotoViewer/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using PhotoViewer.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoViewer
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly List<string> supportExt = new List<string>() { ".jpg", ".jpeg", ".bmp", ".png", ".tif", ".tiff" };
+
+
+
+        private ObservableCollection<Uri> imageList;
+        public ObservableCollection<Uri> ImageList
+        {
+            get { return imageList; }
+            set { this.SetProperty(ref this.imageList, value); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            this.ImageList = new ObservableCollection<Uri>()
+            {
+                new Uri("Images/1.jpg", UriKind.Relative),
+                new Uri("Images/2.jpg", UriKind.Relative),
+                new Uri("Images/3.jpg", UriKind.Relative),
+                new Uri("Images/4.jpg", UriKind.Relative),
+                new Uri("Images/5.jpg", UriKind.Relative),
+            };
+        }
+
+
+
+        public void SetSourceDirectory(string path)
+        {
+            var isExist = Directory.Exists(path);
+            if (!isExist) { return; }
+
+            var files = this.GetFiles(path, supportExt)
+                            .Select(o => new Uri(o.FullName));
+            this.ImageList = new ObservableCollection<Uri>(files);
+        }
+
+
+        protected IEnumerable<FileInfo> GetFiles(string dir, IEnumerable<string> supportExts)
+        {
+            if (!Directory.Exists(dir))
+            {
+                // ディレクトリが無いときは、空リストを返す。
+                return new List<FileInfo>();
+            }
+
+            var dirInfo = new DirectoryInfo(dir);
+            return dirInfo.GetFiles()
+                          .Where(f => supportExts.Contains(f.Extension, StringComparer.OrdinalIgnoreCase))
+                          .OrderBy(f => f.Name);
+        }
+    }
+}
